9b7f30400cec23b79ab277b4b3226c88f98616e9
Lara Hara <lara.hara@aluno.senai.br>
2025-04-08 10:23:50 -0300

    alteração do git.js

---
+++ git.js
@@ -0,0 +1,8 @@
+let comidas = ["lasanha" ,  "pizza" , "suchi" , "hamburguer" , "batata-frita"
+
+]
+
+console.log(`todas as comidas` , comidas)
+
+comidas.sort()
+console.log(`comidas ordenadas` , comidas)
