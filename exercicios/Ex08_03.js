1f13b0f85431be08bed927acf6ad4043e5f97e32
Lara Hara <lara.hara@aluno.senai.br>
2025-04-14 12:18:39 -0300

    aula14 atual

---
+++ exercicios/Ex08_03.js
@@ -0,0 +1,14 @@
+//Crie uma lista com os meses e imprima apenas os meses que começam com "J".
+const prompt = require('prompt-sync')();
+
+let letra = 'j'
+let meses = ["Janeiro", "Fevereiro", "Março", "Abril","Maio", "Junho", "Julho", "Agosto","Setembro", "Outubro", "Novembro", "Dezembro"];
+
+for(let i = 0; i < meses.length; i++){
+
+    console.log(meses[i]);
+
+    if (meses[i][0] === letra){
+        console.log(meses[i]);
+    }
+}
